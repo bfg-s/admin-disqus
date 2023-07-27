8bf8771f4989381bd1ef70000447812a895ca12c
xsaven <gms.sw.xsaven@gmail.com>
2023-07-27 12:43:41 +0300

    first commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AdminDisqus;
+
+use Illuminate\Support\ServiceProvider as ServiceProviderIlluminate;
+
+class ServiceProvider extends ServiceProviderIlluminate
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
